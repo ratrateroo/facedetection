34d1f46bed0e67403b29db08e6d35d16c49bec9a
Mark T <ratrateroo@gmail.com>
2019-03-16 14:31:26 +0800

    changed url

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ class App extends Component {
   }
 
   componentDidMount(){
-    fetch('http://localhost:3000')
+    fetch('https://mysterious-island-96690.herokuapp.com')
     .then(response => response.json())
     .then(console.log)
     .catch(error => console.log('parsing failed', error))
@@ -88,7 +88,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input})
-      fetch('http://localhost:3000/imageurl',{
+      fetch('https://mysterious-island-96690.herokuapp.com/imageurl',{
             method: 'post',
             headers:{'Content-Type':'application/json'},
             body: JSON.stringify({
@@ -98,7 +98,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image',{
+          fetch('https://mysterious-island-96690.herokuapp.com/image',{
             method: 'put',
             headers:{'Content-Type':'application/json'},
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends React.Component{
 
 	onSubmitSignIn = () => {
 		console.log(this.state);
-		fetch('http://localhost:3000/register',{
+		fetch('https://mysterious-island-96690.herokuapp.com/register',{
 			method: 'post',
 			headers:{'Content-Type':'application/json'},
 			body: JSON.stringify({
